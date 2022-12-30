3e3095ec46a9709d9162bdf490121377eb710852
rxc <2585692124@qq.com>
2022-12-30 14:41:23 +0800

    child comp

---
+++ src/components/ChildComp.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>这是我们的子组件</div>
+</template>
+
+<script>
+export default {
+    name: 'ChildComp',
+    // 生命周期函数
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    created() {
+        console.log('created')
+    },
+    beforeMount() {
+        console.log('beforeMount')
+    },
+    mounted() {
+        window.addEventListener('click', this.handleEvent)
+        // TODO: 初始化组件的数据
+        // 异步逻辑
+        // const res = await xxxx();
+        console.log('mounted')
+    },
+    beforeDestroy() {
+        console.log('beforeDestroy')
+    },
+    destroyed() {
+        // 取消事件的订阅
+        console.log('destroyed')
+        window.removeEventListener('click', this.handleEvent)
+    },
+    methods: {
+        handleEvent() {
+            console.log('吱吱吱')
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
