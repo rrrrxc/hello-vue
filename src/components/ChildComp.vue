e01032a08b2bf9e0d7c190445ba5cf41a47a0e24
rxc <2585692124@qq.com>
2022-12-30 16:03:24 +0800

    computed 和 watch

--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -1,10 +1,41 @@
 <template>
-    <div>这是我们的子组件</div>
+   <div>
+    <el-input v-model="message"></el-input>
+    <el-input v-model="name"></el-input>
+
+    <div>反转之后的值：{{ reverseMsg }}</div>
+   </div> 
 </template>
 
 <script>
 export default {
     name: 'ChildComp',
+
+    data: function() {
+        return {
+          message :'',
+          name:''
+        }
+    },
+
+    computed : {
+        reverseMsg:function () {
+            return this.message.split('').reverse().join('')
+        }
+    },
+
+    watch : {
+        name:function (newValue,oldValue) {
+            console.log(newValue,oldValue)
+        },
+        message: {
+            handler : function (newValue,oldValue) 
+            {
+                console.log('message',newValue,oldValue)
+            },
+            immediate:true
+        }
+    },
     // 生命周期函数
     beforeCreate() {
         console.log('beforeCreate')
