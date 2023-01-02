ee80a3aee3948f2f55695e6ca1184b5e0cec0772
rxc <2585692124@qq.com>
2023-01-02 16:25:00 +0800

    组件中函数传递

---
+++ src/components/BallT.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <el-input v-model="arrItem"></el-input>
+        <el-button @click="sendData">向父组件添加数据</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'BallT',
+    props : ['dataObject','dataMethod'],
+    data :function () {
+        return {
+            arrItem:''
+        }
+    },
+    methods :{
+        sendData () {
+            //方法一 react当中父子组件通信方式
+            this.dataMethod(this.arrItem)
+
+           
+        }
+    }
+}
+</script>
