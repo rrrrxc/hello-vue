6bd7fc5d83460632a32c1a1e0392df7feb5d91f8
rxc <2585692124@qq.com>
2023-01-02 14:26:05 +0800

    父子踢球

---
+++ src/components/BallC.vue
@@ -0,0 +1,28 @@
+<template>
+    <div></div>
+</template>
+
+
+<script>
+
+export default {
+    name : 'BallC',
+    props : ['ball'],
+    mounted () {
+        //起点
+        this.$emit('play-ball',this.ball + 1)
+    },
+    watch : {
+        ball :function(newValue) {
+            //减速
+            console.log('父级踢给子集的是',newValue)
+            console.log('BallC减速一下,三秒后再次踢球')
+            
+            setTimeout (()=> { 
+                this.$emit('play-ball',newValue + 1)
+            },3000)
+           
+            }   
+        }
+}
+</script>
---
+++ src/components/BallP.vue
@@ -0,0 +1,26 @@
+<template>
+
+<ball-c :ball="inner" @play-ball="getChild"></ball-c>
+</template>
+
+<script>
+import BallC from './BallC.vue'
+
+export default {
+    name : 'BallP',
+    components : {
+        BallC
+    },
+    data :function () {
+        return {
+            inner :0
+        }
+    },
+    methods : {
+        getChild(ball) {
+            console.log('子级踢给父级的是'+ball)
+            this.inner=ball+1;
+        }
+    }
+}
+</script>
